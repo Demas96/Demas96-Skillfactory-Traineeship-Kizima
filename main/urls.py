a0e9b82c9629199518ab617b0cde2844e30e315a
Demas96 <dmitrijjkizima@yandex.ru>
2022-06-26 19:37:47 +0300

    Вынес метод submitData из класса, вызывается при post запросе

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('submitData', PerevalAPIView.as_view()),
+    path('api/', PerevalAPIView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -7,56 +7,59 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from rest_framework.views import APIView
 
+def submitData(request):
+    try:
+        json_body = json.loads(request.body)
+        user = json_body['user']
+        coords = json_body['coords']
+        level = json_body['level']
+        images = json_body['images']
+        if Users.objects.filter(email=user['email']).exists():
+            Users.objects.filter(email=user['email']).update(firstname=user['name'],
+                                                             lastname=user['fam'], patronymic=user['otc'],
+                                                             phone=user['phone'])
+            uss = Users.objects.get(email=user['email'])
+        else:
+            us = User.objects.create(username=user['email'])
+            uss = Users.objects.create(user=us, email=user['email'], firstname=user['name'],
+                                       lastname=user['fam'], patronymic=user['otc'],
+                                       phone=user['phone'])
+
+        co = Coords.objects.create(latitude=coords['latitude'], longitude=coords['longitude'],
+                                   height=coords['height'])
+        pe = PerevalAdd.objects.create(coords=co, beautyTitle=json_body['beauty_title'],
+                                       title=json_body['title'], other_titles=json_body['other_titles'],
+                                       connect=json_body['connect'], add_time=json_body['add_time'],
+                                       level_winter=level['winter'], level_summer=level['summer'],
+                                       level_autumn=level['autumn'], level_spring=level['spring'],
+                                       user=uss)
+
+        for obj in images:
+            Images.objects.create(pereval=pe, img=obj['data'], title=obj['title'])
+        data = {
+            'status': '200',
+            'message': 'null',
+            'id': f"{pe.id}"
+        }
+        return data
+
+    except Exception as exs:
+        data = {
+            'status': '500',
+            'message': f'{exs}',
+            'id': 'null'
+        }
+        return data
+
+    except KeyError as exs:
+        data = {
+            'status': '400',
+            'message': 'Не хватает полей',
+            'id': 'null'
+        }
+        return data
 
 class PerevalAPIView(APIView):
     def post(self, request):
-        try:
-            json_body = json.loads(request.body)
-            user = json_body['user']
-            coords = json_body['coords']
-            level = json_body['level']
-            images = json_body['images']
-            if Users.objects.filter(email=user['email']).exists():
-                Users.objects.filter(email=user['email']).update(firstname=user['name'],
-                                           lastname=user['fam'], patronymic=user['otc'],
-                                           phone=user['phone'])
-                uss = Users.objects.get(email=user['email'])
-            else:
-                us = User.objects.create(username=user['email'])
-                uss = Users.objects.create(user=us, email=user['email'], firstname=user['name'],
-                                           lastname=user['fam'], patronymic=user['otc'],
-                                           phone=user['phone'])
-
-            co = Coords.objects.create(latitude=coords['latitude'], longitude=coords['longitude'],
-                                       height=coords['height'])
-            pe = PerevalAdd.objects.create(coords=co, beautyTitle=json_body['beauty_title'],
-                                           title=json_body['title'], other_titles=json_body['other_titles'],
-                                           connect=json_body['connect'], add_time=json_body['add_time'],
-                                           level_winter=level['winter'], level_summer=level['summer'],
-                                           level_autumn=level['autumn'], level_spring=level['spring'],
-                                           user=uss)
-
-            for obj in images:
-                Images.objects.create(pereval=pe, img=obj['data'], title=obj['title'])
-            data = {
-                'status': '200',
-                'message': 'null',
-                'id': f"{pe.id}"
-            }
-            return JsonResponse(data, status=200)
-
-        except Exception as exs:
-            data = {
-                'status': '500',
-                'message': f'{exs}',
-                'id': 'null'
-            }
-            return JsonResponse(data, status=500)
-
-        except KeyError as exs:
-            data = {
-                'status': '400',
-                'message': 'Не хватает полей',
-                'id': 'null'
-            }
-            return JsonResponse(data, status=400)
+        data = submitData(request)
+        return JsonResponse(data, status=data['status'])
